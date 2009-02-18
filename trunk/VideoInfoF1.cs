edc0fadeb29a98e2dd96d1ac39e461c415e56273
Mike.Thiels <Mike.Thiels@659517ca-8d8e-11dd-a205-0939b8ba2825>
2009-02-18 17:59:18 +0000

    Merge code from Mike Thiels (part 1-new stuff). 1) Add Video Library handling. 2) Add sorting to music play list. 3) Add Save/Load of music play list.

---
+++ trunk/VideoInfoF1.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace XBMControl
+{
+    public partial class videoInfoF1 : Form
+    {
+        MainForm parent;
+        string[] videoInfoList;
+
+        public videoInfoF1(MainForm parentForm, string videoID)
+        {
+            parent = parentForm;
+            InitializeComponent();
+            this.Owner = parent;
+            this.fillVideoInfo(videoID);
+        }
+
+        private void bClose_Click(object sender, EventArgs e)
+        {
+            parent.videoInfoOpened = false;
+            this.Close();
+        }
+
+        private void fillVideoInfo(string videoID)
+        {
+            videoInfoList = parent.XBMC.Video.GetVideoLibraryInfo(videoID);
+
+            this.videoPicture.Image = parent.XBMC.Video.getVideoThumb(videoID);
+            this.videoName.Text = videoInfoList[1];
+            this.videoPlot.Text = videoInfoList[2];
+            this.videoGenre.Text = videoInfoList[15];
+            this.videoYear.Text = videoInfoList[8];
+            this.videoRuntime.Text = videoInfoList[12];
+            this.videoRating.Text = videoInfoList[6] + " (" + videoInfoList[5] + " votes)";
+            this.videoTagline.Text = videoInfoList[4];
+            this.videoMPAARating.Text = videoInfoList[13];
+        }
+    }
+}
